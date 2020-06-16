66a68aa1f68819a9c838859b119f0b6528e06268
LEEYEONJAE <eunoiaa22@gmail.com>
2020-06-16 23:40:36 +0900

    Modify WordItem

--- app/src/main/java/com/yeonproject/dodam_mvvm/data/model/WordItem.kt
+++ app/src/main/java/com/yeonproject/dodam_mvvm/data/model/WordItem.kt
@@ -2,7 +2,7 @@ package com.yeonproject.dodam_mvvm.data.model
 
 import java.io.Serializable
 
-data class WordItem(
+data class WordItem (
     val name: String = "",
     val image: String = ""
 ) : Serializable
